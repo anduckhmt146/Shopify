4388f34ce653220f6bcf07f7bb6195dce84465b1
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2023-09-04 22:44:56 +0700

    update sign up

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -5,7 +5,12 @@ const {
   createUser,
 } = require('../models/user.model');
 
-const { NotFoundError } = require('../utils/errorHandler');
+const {
+  NotFoundError,
+  UnauthenticatedError,
+} = require('../utils/errorHandler');
+
+const { hashPassword, comparePassword } = require('../utils/passwordService');
 
 const GetAllUsers = async (req: any, res: any) => {
   const data = await getAllUsers();
@@ -15,21 +20,34 @@ const GetAllUsers = async (req: any, res: any) => {
 const GetUserById = async (req: any, res: any, next: any) => {
   const data = await getUserById(req.params.id);
   if (data.length === 0) {
-    const notFoundError = new NotFoundError('Not found UserID');
+    const notFoundError = new NotFoundError('User not found');
     next(notFoundError);
     return;
   }
   return res.send(data);
 };
 
-const FindByUserName = async (req: any, res: any) => {
+const FindByUserName = async (req: any, res: any, next: any) => {
   const userName = req.body.username;
   const data = await findByUserName(userName);
-  if (data.length === 0) throw new NotFoundError('User not found');
+  if (data.length === 0) {
+    const notFoundError = new NotFoundError('User not found');
+    next(notFoundError);
+    return;
+  }
   return res.send(data);
 };
 
-const Register = async (req: any, res: any) => {
+const Register = async (req: any, res: any, next: any) => {
+  const userName = req.body.username;
+  const userIsExist = await findByUserName(userName);
+  if (userIsExist.length !== 0) {
+    const unAuthenticatedError = new UnauthenticatedError('User is exist');
+    next(unAuthenticatedError);
+    return;
+  }
+  // hash password
+  req.body.password = hashPassword(req.body.password);
   const data = await createUser(req.body);
   return res.send(data);
 };
